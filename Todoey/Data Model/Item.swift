a69f7c15778255375ce5b0f77808ff1d717f647d
Marco Riesgo <marcoriesgo@marcos-macbook-pro.local>
2019-02-21 15:31:55 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,19 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Marco Riesgo Almonacid on 2/18/19.
+//  Copyright © 2019 Marco Riesgo. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    
+    var done: Bool = false
+    
+    
+    
+}
